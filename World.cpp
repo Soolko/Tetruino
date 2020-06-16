4e7cc9a9aa7c390e8214e7c9a9b43ae96bc7575d
Snorklo <37601414+Soolko@users.noreply.github.com>
2020-06-16 13:36:56 +0100

    Line clear check code

--- World.cpp
+++ World.cpp
@@ -1,4 +1,5 @@
 #include "World.hpp"
+#include "Vector.hpp"
 
 using namespace Tetruino;
 
@@ -32,6 +33,67 @@ void World::addBlock(const Block& block)
 			blockMap[collisionX + (collisionY * bounds.width)] = block.colour;
 		}
 	}
+}
+
+size_t World::checkLines(uint8_t* lines)
+{
+	// Store the lines
+	Vector<uint8_t>* vector = nullptr;
+	size_t size = 0;
+	
+	for(unsigned char y = bounds.height - 1; y >= 0; y--)
+	{
+		unsigned char hitCount = 0;
+		for(unsigned char x = 0; x < bounds.width; x++)
+		{
+			if(blockMap[x + (y * bounds.width)] == nullptr) break;
+			else hitCount++;
+		}
+		
+		// Nothing hit, therefore there can't be anything above here,
+		// so we can exit out of the loop.
+		if(hitCount == 0) break;
+		
+		// This line is full.
+		if(hitCount == bounds.width)
+		{
+			size += 1;
+			if(vector == nullptr)
+			{
+				// If vector not initialised, initialise it.
+				vector = new Vector<uint8_t>(y);
+			}
+			else
+			{
+				// Go to end of vector
+				auto* current = vector;
+				while(current->next != nullptr) current = vector->next;
+				
+				// Add to vector
+				current->next = new Vector<uint8_t>(y);
+			}
+		}
+	}
+	
+	// Nothing to do here
+	if(size == 0) return 0;
+	
+	// Convert to array
+	lines = new uint8_t[size];
+	
+	uint8_t i = 0;
+	auto* current = vector;
+	while(current->next != nullptr)
+	{
+		lines[i++] = current->value;
+		current = current->next;
+	}
+	
+	// Free up
+	delete vector;
+	
+	// Return
+	return size;
 }
 
 bool World::hitBottom(const Block& block)
